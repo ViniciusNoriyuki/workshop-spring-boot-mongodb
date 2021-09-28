a4f1efaee51b8bb24e7bf1c6fa0ceb9c9e060c5b
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-28 14:59:50 -0300

    Conectando ao MongDB com Repository e Service

--- src/main/java/com/noriyuki/workshopmongo/domain/User.java
+++ src/main/java/com/noriyuki/workshopmongo/domain/User.java
@@ -3,15 +3,19 @@ package com.noriyuki.workshopmongo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/noriyuki/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.noriyuki.workshopmongo.services;
+
+import com.noriyuki.workshopmongo.domain.User;
+import com.noriyuki.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
