9fd3845299e0f1c7ec9273f541d2595e137093a0
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-28 15:14:32 -0300

    Usando padrao DTO para retornar usuarios

---
+++ src/main/java/com/noriyuki/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.noriyuki.workshopmongo.dto;
+
+import com.noriyuki.workshopmongo.domain.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+}
