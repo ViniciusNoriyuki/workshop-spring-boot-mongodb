09468ff939b301859fd41d61564029b0dd49a9b9
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-29 11:29:37 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/noriyuki/workshopmongo/domain/Post.java
@@ -0,0 +1,25 @@
+package com.noriyuki.workshopmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Post implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+}
---
+++ src/main/java/com/noriyuki/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.noriyuki.workshopmongo.repository;
+
+import com.noriyuki.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
