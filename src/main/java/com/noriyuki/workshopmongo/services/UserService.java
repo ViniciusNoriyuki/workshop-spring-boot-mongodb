80fa35da0c9a278db82b55ac49a1650a79cc4f7d
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-29 10:01:40 -0300

    Insercao de usuario com POST

--- src/main/java/com/noriyuki/workshopmongo/services/UserService.java
+++ src/main/java/com/noriyuki/workshopmongo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.noriyuki.workshopmongo.services;
 
 import com.noriyuki.workshopmongo.domain.User;
+import com.noriyuki.workshopmongo.dto.UserDTO;
 import com.noriyuki.workshopmongo.repository.UserRepository;
 import com.noriyuki.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +25,14 @@ public class UserService {
         Optional<User> obj = userRepository.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    @Transactional
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
