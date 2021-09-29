76bf134a2aebfa68e9e4a3e7546ad15a2ffde8b1
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-29 13:00:46 -0300

    Obtendo post por ID

---
+++ src/main/java/com/noriyuki/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.noriyuki.workshopmongo.services;
+
+import com.noriyuki.workshopmongo.domain.Post;
+import com.noriyuki.workshopmongo.repository.PostRepository;
+import com.noriyuki.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
--- src/main/java/com/noriyuki/workshopmongo/services/UserService.java
+++ src/main/java/com/noriyuki/workshopmongo/services/UserService.java
@@ -54,5 +54,4 @@ public class UserService {
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
-
 }
