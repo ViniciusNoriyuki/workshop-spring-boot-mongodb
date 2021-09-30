1471dbbcbae67e6c741168dd165a77d5ab073782
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-30 16:38:12 -0300

    Adicionada validacao para email

--- src/main/java/com/noriyuki/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/noriyuki/workshopmongo/dto/UserDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
@@ -18,6 +19,9 @@ public class UserDTO implements Serializable {
     @NotEmpty(message = "Preenchimento obrigatório")
     @Length(min = 3, max = 80, message = "O tamanho deve ser entre 3 e 80 caracteres")
     private String name;
+
+    @NotEmpty(message = "Preenchimento obrigatório!")
+    @Email(message = "Email inválido!")
     private String email;
 
     public UserDTO(User obj) {
