4deed5543ff8e97b1bdae882fad923d1b5780240
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-10-01 09:50:29 -0300

    Configuracao inicial do Spring Security

---
+++ src/main/java/com/noriyuki/workshopmongo/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.noriyuki.workshopmongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String[] PUBLIC_MATCHERS_GET = {
+            "/users/**",
+            "/posts/**"
+    };
+
+    public static final String[] PUBLIC_MATCHERS_POST = {
+            "/users"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable(); // Aplica as configurações do Cors e desabilita proteção csrf
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());    // Permite acesso básico de múltpilas fontes para todos caminhos
+        return source;
+    }
+}
